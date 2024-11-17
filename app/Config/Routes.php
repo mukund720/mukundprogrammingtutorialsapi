89100f421b11777c21881bc60058cb6dc0bc5d64
Mukund Kumar <mukund720@gmail.com>
2024-11-17 16:21:38 +0530

    changes for login api

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,6 +12,6 @@ $routes->group('api', function($routes) {
     $routes->post('users', 'ApiController::create');
     $routes->put('users/(:num)', 'ApiController::update/$1');
     $routes->delete('users/(:num)', 'ApiController::delete/$1');
-    $routes->post('users', 'ApiController::login');
+    $routes->post('users/login', 'ApiController::login');
 
 });
